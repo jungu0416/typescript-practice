9da81a51b09a30d231fa8b4e94b3e454b2bf1ea0
jungu0416 <jungu0416@naver.com>
2025-02-23 21:39:07 +0900

    03.

---
+++ 03. type-assertion, type-guard/build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// const bodyElement = document.querySelector("body") as HTMLBodyElement;
+// bodyElement.innerText = "Hello";
+const bodyElement = document.querySelector("body");
+bodyElement.innerText = "Hello";
+// function func(arg: string | null) {
+//   if (arg) {
+//     return arg.toLowerCase();
+//   }
+// }
+// func("Hello");
+// func(null);
---
+++ 03. type-assertion, type-guard/src/main.ts
@@ -0,0 +1,14 @@
+// const bodyElement = document.querySelector("body") as HTMLBodyElement;
+// bodyElement.innerText = "Hello";
+
+const bodyElement = document.querySelector("body");
+bodyElement!.innerText = "Hello";
+
+// function func(arg: string | null) {
+//   if (arg) {
+//     return arg.toLowerCase();
+//   }
+// }
+
+// func("Hello");
+// func(null);
